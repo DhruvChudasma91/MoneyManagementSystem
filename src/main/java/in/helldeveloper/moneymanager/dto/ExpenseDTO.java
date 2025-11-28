a41be9376f2b287e80cc5b60c30042a3e0facb74
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2025-11-28 09:10:44 +0530

    feat: implement IncomeRepository and ExpenseRepository with DTO support

---
+++ src/main/java/in/helldeveloper/moneymanager/dto/ExpenseDTO.java
@@ -0,0 +1,28 @@
+package in.helldeveloper.moneymanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExpenseDTO {
+
+    private Long id;
+    private String name;
+    private String icon;
+    private Long categoryName;
+    private Long categoryId;
+
+    private BigDecimal amount;
+    private LocalDate date;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/in/helldeveloper/moneymanager/repository/IncomeRepository.java
@@ -0,0 +1,37 @@
+package in.helldeveloper.moneymanager.repository;
+
+import in.helldeveloper.moneymanager.entity.ExpenseEntity;
+import in.helldeveloper.moneymanager.entity.IncomeEntity;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface IncomeRepository extends JpaRepository<IncomeEntity, Long> {
+
+    //SELECT * FROM tbl_incomes WHERE profile_id = ? ORDER BY date DESC
+    List<IncomeEntity> findByProfileIdOrderByDateDesc(Long profileId);
+
+    //SELECT * FROM tbl_incomes WHERE profile_id = ? ORDER BY date DESC LIMIT 5
+    List<IncomeEntity> findTop5ByProfileIdOrderByDateDesc(Long profileId);
+    @Query("SELECT SUM(e.amount) FROM IncomeEntity e WHERE e.profile.id = :profileId")
+    BigDecimal findTotalExpenseByProfileId(@Param("profileId") Long profileId);
+
+    //SELECT * FROM tbl_incomes WHERE profile_id = ? AND date BETWEEN ? AND ? AND name LIKE %?%
+    List<IncomeEntity> findByProfileIdAndDateBetweenAndNameContainingIgnoreCase(
+            Long profileId,
+            LocalDate startDate,
+            LocalDate endDate,
+            String keyword,
+            Sort sort
+    );
+
+    //SELECT * FROM tbl_incomes WHERE profile_id = ? AND date BETWEEN ? AND ?
+    List<IncomeEntity> findByProfileIdAndDateBetween(Long profileId, LocalDate startDate, LocalDate endDate);
+}
