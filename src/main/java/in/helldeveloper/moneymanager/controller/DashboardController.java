2c1b889588a9d163d2b7a54f11181acd92874e4f
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-09 10:51:55 +0530

    feat: Updated Cross Origin

--- src/main/java/in/helldeveloper/moneymanager/controller/DashboardController.java
+++ src/main/java/in/helldeveloper/moneymanager/controller/DashboardController.java
@@ -3,6 +3,7 @@ package in.helldeveloper.moneymanager.controller;
 import in.helldeveloper.moneymanager.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/dashboard")
 public class DashboardController {
 
--- src/main/java/in/helldeveloper/moneymanager/controller/EmailController.java
+++ src/main/java/in/helldeveloper/moneymanager/controller/EmailController.java
@@ -3,7 +3,6 @@ package in.helldeveloper.moneymanager.controller;
 import in.helldeveloper.moneymanager.entity.ProfileEntity;
 import in.helldeveloper.moneymanager.service.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
